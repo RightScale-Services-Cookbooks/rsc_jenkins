817c9d27edfe92c0a34229ff002108c52ed48ad1
Stefhen Hovland <stefhen@rightscale.com>
2015-12-01 18:18:44 -0500

    Install java

--- recipes/_software.rb
+++ recipes/_software.rb
@@ -1,4 +1,4 @@
-%w[make patch gcc g++ subversion mercurial git maven].each do |pkg|
+%w[openjdk-7-jre-headless make patch gcc g++ subversion mercurial git maven].each do |pkg|
   package pkg do
     action :install
   end

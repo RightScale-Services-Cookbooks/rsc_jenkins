7fa0eb0263cc793d1f4a965321342185c102feb6
Stefhen Hovland <stefhen@rightscale.com>
2015-12-01 18:18:09 -0500

    Install software deps before installing Jenkins

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,6 +2,6 @@ node.default['jenkins']['master']['install_method'] = 'war'
 node.default['jenkins']['master']['source'] = node['rsc_jenkins']['war_url']
 node.default['jenkins']['master']['checksum'] = node['rsc_jenkins']['checksum']
 
+include_recipe 'rsc_jenkins::_software'
 include_recipe 'jenkins::master'
 include_recipe 'rsc_jenkins::_user'
-include_recipe 'rsc_jenkins::_software'
